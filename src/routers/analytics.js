5b27200e9db9b83ae8b93b686f7bf431b4e4adcb
Akhilesh m t <akhileshthykkat843@gmail.com>
2024-12-27 22:57:30 +0530

    added rate limiting for creating shor url and listing analytics

--- src/routers/analytics.js
+++ src/routers/analytics.js
@@ -2,21 +2,25 @@ const express = require("express");
 const router = express.Router();
 
 const authenticateJWT = require("../middlewares/checkToken");
+const { rateLimiter } = require("../middlewares/rateLimiter");
 const { analytics_controller } = require("../controllers");
 
 router.get(
   "/api/analytics/overall",
   authenticateJWT,
+  rateLimiter.analyticsRateLimiter,
   analytics_controller.overallAnalytics
 );
 router.get(
   "/api/analytics/:alias",
   authenticateJWT,
+  rateLimiter.analyticsRateLimiter,
   analytics_controller.analyticsByAlias
 );
 router.get(
   "/api/analytics/topic/:topic",
   authenticateJWT,
+  rateLimiter.analyticsRateLimiter,
   analytics_controller.analyticsByTopic
 );
 
